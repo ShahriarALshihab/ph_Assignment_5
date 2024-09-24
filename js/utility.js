6123ee9b3c6e33018d459c5cb08b113d27cd15fa
shihab <shahriarshihab96@gmail.com>
2024-09-25 05:35:21 +0600

    recheck

--- js/utility.js
+++ js/utility.js
@@ -15,21 +15,15 @@ function getInputValue(id) {
 
 
    function getCurrentDateAndTime() {
-    const now = new Date();
-    const options = {
-        timeZone: 'Asia/Dhaka',
+       const now = new Date();
        
-    };
-
-    const dateString = now.toLocaleString('en-US', options).replace(',', '');
-    const timeString = now.toTimeString().split(' ')[0];
-    const dayString = now.toDateString();
-    
-    return ` ${dayString} ${timeString} GMT+0600 (Bangladesh Standard Time)`;
+    const detailTime = now.toTimeString();
+    const date = now.toDateString();
+       return `${date} ${detailTime}`; 
+   
 }
- document.addEventListener("DOMContentLoaded", function () {
+ 
     const navbar = document.querySelector('nav');
-
     window.addEventListener('scroll', function () {
       if (window.scrollY > 50) {
         navbar.classList.add('backdrop-blur-md', 'bg-opacity-10');
@@ -37,7 +31,7 @@ function getInputValue(id) {
         navbar.classList.remove('backdrop-blur-md', 'bg-opacity-10'); 
       }
     });
-  });
+  
        
 function getTitle(id) {
     return document.getElementById(id).innerText; 
