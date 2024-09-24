d946a4e8779961d1541c9ed59eb19c70889b40fd
shihab <shahriarshihab96@gmail.com>
2024-09-24 21:42:17 +0600

    donation part manipulation done

--- js/domManipulation.js
+++ js/domManipulation.js
@@ -1,14 +1,192 @@
-const totalBalance = getBalanceById('#total-balance'); 
+let totalBalance = getBalanceById('total-balance'); 
+const noakhaliDonateButton = catchElement("btn-noakhali"); 
+const feniDonateButton = catchElement("btn-feni"); 
+const quotaDonateButton = catchElement("btn-quota"); 
+const donationModalToggle = catchElement("donation-modal"); 
 
-const noakhaliInputField = document.querySelector('#donate-noakhali'); 
-noakhaliInputField.addEventListener("click", function (e) {
-    console.log("clk");
+
+
+
+
+noakhaliDonateButton.addEventListener("click", function () {
+
+    let previousNoakhaliBalance = getBalanceById("previous-noakhali-amount"); 
+
+    const noakhaliInputValue = getInputValue("donate-noakhali"); 
+
+    if (isNaN(noakhaliInputValue) || noakhaliInputValue <= 0) {
+        return alert("Invalid input"); 
+    }
+
+    if (noakhaliInputValue > totalBalance) {
+        alert("Insufficient Balance"); 
+        catchElement("donate-noakhali").value = "";
+        return; 
+        }
+    const newCardAmount = previousNoakhaliBalance + noakhaliInputValue; 
+  
+  
+    document.getElementById("previous-noakhali-amount").innerText = newCardAmount.toFixed(2); 
+    
+ totalBalance -= noakhaliInputValue;
+
+    document.getElementById("total-balance").innerText = totalBalance.toFixed(2)
+
+    document.getElementById("donate-noakhali").value = ""; 
+
+
+
+
+    //add history item
+
+    const historyItem = document.createElement("div"); 
+    
+    const title = getTitle("noakhali-title"); 
+    historyItem.className = "border border-gray-400 text-black p-4 mx-16 mt-6 rounded-md"; 
+    const dateTime = getCurrentDateTime();
+       historyItem.innerHTML = `
+      <p class="text-lg font-bold">
+        ${noakhaliInputValue} Taka is ${title}
+        </p>
+        <p class="bg-white py-5 pl-6">Last Donation on: ${dateTime}</p>
+
+`
+
+    const historyContainer = catchElement("history-part"); 
+
+historyContainer.insertBefore(historyItem, historyContainer.firstChild);  
+
+    //toggle modal when clicked
+
+    donationModalToggle.checked = true;
+
+
+ 
+});
+
+
+
+
+feniDonateButton.addEventListener("click", function () {
+    
+     let previousFeniBalance = getBalanceById("previous-feni-amount"); 
+
+    const feniInputValue = getInputValue("donate-feni"); 
+
+    if (isNaN(feniInputValue) || feniInputValue <= 0) {
+        return alert("Invalid input"); 
+    }
+    if (feniInputValue > totalBalance) {
+        alert("Insufficient Balance"); 
+        catchElement("donate-feni").value = "";
+        return; 
+        }
+    const newCardAmount = previousFeniBalance + feniInputValue; 
+  
+  
+    document.getElementById("previous-feni-amount").innerText = newCardAmount.toFixed(2); 
+    
+     totalBalance -= feniInputValue;
+
+    document.getElementById("total-balance").innerText = totalBalance.toFixed(2); 
+    document.getElementById("donate-feni").value = ""; 
+
+
+
+
+    //add history item
+
+    const historyItem = document.createElement("div"); 
+    
+    const title = getTitle("feni-title"); 
+
+    historyItem.className = "border border-gray-400 text-black p-4 mx-16 mt-6 rounded-md"; 
+    const dateTime = getCurrentDateTime();
+       historyItem.innerHTML = `
+      <p class="text-lg font-bold">
+        ${feniInputValue} Taka is ${title}
+        </p>
+        <p class="bg-white py-5 pl-6">Last Donation on: ${dateTime}</p>
+
+`
+
+    const historyContainer = catchElement("history-part"); 
+
+historyContainer.insertBefore(historyItem, historyContainer.firstChild);  
+
+    //toggle modal when clicked
+
+    donationModalToggle.checked = true;
+
+
+
 })
 
-const noakhaliDonateButton = document.querySelector("#btn-noakhali").addEventListener('click', function (e) {
+
+
+quotaDonateButton.addEventListener("click", function () {
+    
+     let previousQuotaBalance = getBalanceById("previous-quota-amount"); 
+
+    const quotaInputValue = getInputValue("donate-quota"); 
+
+    if (isNaN(quotaInputValue) || quotaInputValue <= 0) {
+        return alert("Invalid input"); 
+    }
+    if (quotaInputValue > totalBalance) {
+        alert("Insufficient Balance"); 
+        catchElement("donate-quota").value = "";
+        return; 
+    }
+    
+
+    const newCardAmount = previousQuotaBalance + quotaInputValue; 
+  
+  
+    document.getElementById("previous-quota-amount").innerText = newCardAmount.toFixed(2); 
+    
+     totalBalance -= quotaInputValue; 
+
+    document.getElementById("total-balance").innerText =  totalBalance.toFixed(2)
+
+    document.getElementById("donate-quota").value = ""; 
+
+
+
+
+    //add history item
+
+    const historyItem = document.createElement("div"); 
+    
+    const title = getTitle("quota-title"); 
+
+    historyItem.className = "border border-gray-400 text-black p-4 mx-16 mt-6 rounded-md"; 
+
+    const dateTime = getCurrentDateTime();
+
+       historyItem.innerHTML = `
+      <p class="text-lg font-bold">
+        ${quotaInputValue} Taka is ${title}
+        </p>
+        <p class="bg-white py-5 pl-6">Last Donation on: ${dateTime}</p>
+
+`
+
+    const historyContainer = catchElement("history-part"); 
+
+historyContainer.insertBefore(historyItem, historyContainer.firstChild);  
+
+    //toggle modal when clicked
+
+    donationModalToggle.checked = true;
+
+
 
 })
 
+
+
+//toggle history and donate button
 
 
 const historyButton = document.getElementById("history-btn"); 
@@ -39,4 +217,12 @@ donateButton.addEventListener('click', function () {
     document.getElementById("history-part").classList.add("hidden"); 
     document.getElementById("donation-part").classList.remove("hidden"); 
 })
+
+
+
+
+
+
+
+
 
--- js/utility.js
+++ js/utility.js
@@ -1,5 +1,26 @@
+
+
 function getBalanceById(id) {
-    return parseFloat(document.querySelector(id).innerText);  
+    return parseFloat(document.getElementById(id).innerText);  
 }
 
 
+function catchElement(id) {
+    return document.getElementById(id);
+}
+
+function getInputValue(id) {
+    return parseFloat(document.getElementById(id).value); 
+}
+
+
+   function getCurrentDateTime() {
+            const now = new Date();
+            const date = now.toLocaleDateString();
+            const time = now.toLocaleTimeString();
+            return `Date: ${date} | Time: ${time}`;
+}
+        
+function getTitle(id) {
+    return document.getElementById(id).innerText; 
+}
